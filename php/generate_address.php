819d6752f4eb79831d7898df61a91affca5681e8
Bryan Saltzman <bryan@neatquiz.com>
2025-09-19 08:47:11 -0700

    Clean up legacy files from master branch
    - Remove outdated balances.json and join_map.json
    - Remove legacy index/ directory
    - Remove old php/ scripts
    - Remove legacy test files
    - Remove sample transaction files
    - Keep .github/workflows and scripts/ for proper operation

--- php/generate_address.php
+++
@@ -1,9 +0,0 @@
-<?php
-$privateKey = openssl_pkey_new(['private_key_bits' => 2048]);
-openssl_pkey_export($privateKey, $privKeyOut);
-$details = openssl_pkey_get_details($privateKey);
-$pubKey = $details['key'];
-$address = substr(sha1($pubKey), 0, 40);
-echo "Address: $address\n";
-echo "Private Key:\n$privKeyOut";
-echo "Public Key:\n$pubKey";
